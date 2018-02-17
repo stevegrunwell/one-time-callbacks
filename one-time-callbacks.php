16d66be6d643906be9bc03db0a2c2afa8956f332
Steve Grunwell <steve@stevegrunwell.com>
2018-02-17 02:03:28 +0000

    Crib a whole bunch of https://github.com/stevegrunwell/wp-cache-remember for yet another tiny WP library

---
+++ one-time-callbacks.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: One-time Callbacks
+ * Plugin URI:  https://github.com/stevegrunwell/one-time-callbacks
+ * Description: Enable WordPress actions and filter callbacks to be called exactly once.
+ * Author:      Steve Grunwell
+ * Author URI:  https://stevegrunwell.com
+ * Version:     1.0.0
+ *
+ * @package SteveGrunwell\OneTimeCallbacks
+ */
+
---
+++ tests/test-actions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Tests for the WordPress action hooks.
+ *
+ * @package SteveGrunwell\OneTimeCallbacks
+ */
+
+/**
+ * Object cache test case.
+ */
+class ObjectCacheTest extends WP_UnitTestCase {
+
+
+}
